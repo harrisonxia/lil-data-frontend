17d4fc548e3de69a615894066bc44e256e30420b
harrisonx <engcxia@gmail.com>
2018-12-05 12:31:28 -0800

    Updates

---
+++ precache-manifest.7c11379f658315cbec44b451c54a5202.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "120081be07831a49980f",
+    "url": "/lil-data-frontend/static/css/main.b65b17c2.chunk.css"
+  },
+  {
+    "revision": "120081be07831a49980f",
+    "url": "/lil-data-frontend/static/js/main.120081be.chunk.js"
+  },
+  {
+    "revision": "db5cfc088e8312dbd1f3",
+    "url": "/lil-data-frontend/static/js/1.db5cfc08.chunk.js"
+  },
+  {
+    "revision": "3244e7226aed39a21953",
+    "url": "/lil-data-frontend/static/js/runtime~main.3244e722.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lil-data-frontend/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f1171a35e9793dcc4637ad8dee6017f2",
+    "url": "/lil-data-frontend/index.html"
+  }
+];

ddd5ec881cd695dcddb572828e66ddcfdf0313ce
harrisonx <engcxia@gmail.com>
2018-12-05 12:25:53 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,92 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import logo from './logo.svg'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+const MainMenu = () => {
+    return (<div>
+        <Link to="/">
+            <button>home</button>
+        </Link>
+        <Link to="/about">
+            <button>About</button>
+        </Link>
+        <Link to="/code">
+            <button>code</button>
+        </Link>
+        <Link to="/code">
+            <button>contact</button>
+        </Link>
+        <Link to="/info">
+            <button>info</button>
+        </Link>
+    </div>)
 }
 
-export default App;
+
+const Home = () => (
+    <div>
+        Home
+    </div>
+)
+
+
+const About = () => (
+    <div>
+        About
+    </div>
+)
+
+const Code = () => (
+    <div>
+        Code
+    </div>
+)
+
+const Contact = () => (
+    <div>
+        Contact
+    </div>
+)
+const info = () => (
+    <div>
+        info
+    </div>
+)
+
+class App extends Component {
+    render() {
+        return (
+
+            <Router basename='lil-data-frontend'>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <p>
+                            Edit <code>src/App.js</code> and save to reload.
+                        </p>
+                        <a
+                            className="App-link"
+                            href="https://reactjs.org"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Learn React
+                        </a>
+                        <MainMenu/>
+                        <div>
+                            <Route exact path="/" component={Home} />
+                            <Route exact path="/about" component={About} />
+                            <Route exact path="/code" component={Code} />
+                            <Route exact path="/contact" component={Contact} />
+                            <Route exact path="/presence" component={info} />
+                        </div>
+                    </header>
+                </div>
+            </Router>
+        )
+    }
+}
+
+export default App
---
+++ src/test.jsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+const Test = () => {
+    let name = 'Lil Data'
+    let text = 'Gaming Trend Analysis from 2015 to 2018'
+    return (
+        <div>
+            <div>
+                {name}
+            </div>
+            <div>
+                <b>{text}</b>
+            </div>
+        </div>
+
+    )
+}
+
+export default Test
